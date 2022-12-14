113bd5c99894db5cce5b24144e1a75e1f6329536
Aqeel <aqeelsaddique83@gmail.com>
2022-12-14 23:34:47 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import Slider from "react-slick";
+import React from 'react';
 
-function App() {
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import "./Testimonial.css";
+import { Avatar } from "@material-ui/core";
+import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+
+const PreviousBtn = (props) => {
+  console.log(props);
+  const { className, onClick } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={className} onClick={onClick}>
+      <ArrowBackIos style={{ color: "gray", fontSize: "45px" }} />
     </div>
   );
-}
+};
+const NextBtn = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowForwardIos style={{ color: "gray", fontSize: "45px" }} />
+    </div>
+  );
+};
+const Testimonial = () => {
+  return (
+    <div
+      className="testimonial"
+      style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+    >
+      <div style={{ width: "50%", textAlign: "center" }}>
+        <h1 style={{ marginBottom: 20 }}>TESTIMONIALS</h1>
+        <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
+          <Card img="https://www.tutorialrepublic.com/examples/images/clients/1.jpg" />
+          <Card img="https://www.tutorialrepublic.com/examples/images/clients/2.jpg" />
+          <Card img="https://www.tutorialrepublic.com/examples/images/clients/3.jpg" />
+        </Slider>
+      </div>
+    </div>
+  );
+};
 
-export default App;
+const Card = ({ img }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        color: "gray",
+      }}
+    >
+      <Avatar
+        imgProps={{ style: { borderRadius: "50%" } }}
+        src={img}
+        style={{
+          width: 120,
+          height: 120,
+          border: "1px solid lightgray",
+          padding: 7,
+          marginBottom: 20,
+        }}
+      />
+      <p>
+        Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia.
+        Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan
+        tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt
+        fringilla massa. Etiam hendrerit dolor eget rutrum
+      </p>
+      <p style={{ fontStyle: "italic", marginTop: 25 }}>
+        <span style={{ fontWeight: 500, color: "green" }}>PAULA WILSON</span> ,
+        Media Analyst
+      </p>
+    </div>
+  );
+};
+
+export default Testimonial;
---
+++ src/testinomial.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const testinomial = () => {
+  return (
+    <div>testinomial</div>
+  )
+}
+
+export default testinomial
